7be4116771c5f5d5e809942ae2acad06ed097bf6
ansh-prasad <anshprasad01@gmail.com>
2024-05-21 11:07:31 +0530

    Nav Bar Added

--- Projects/Book Store App/Frontend/src/components/Navbar.jsx
+++ Projects/Book Store App/Frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function Navbar() {
+  const navItems = (
+   <>
+    <li><a>Home</a></li>
+      <li><a>Course</a></li>
+      <li><a>Contect</a></li>
+      <li><a>About</a></li>
+   </>
+  );
   return (
-    <div>
+    <div className="max-w-screen-2x1 cintainer mx-auto md:px-20 px-4">
         <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
@@ -10,37 +18,36 @@ function Navbar() {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {navItems}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
+    <a className=" fornt-bold curser-pointer btn btn-ghost text-2xl">Book Store</a>
+  </div> 
+  <div className="navbar-end space-x-3">
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <ul className="menu menu-horizontal px-1">{navItems}
+     
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
+  <div className='hidden md:block'>
+    <label className="px-3  py-2 border rounded-md flex rounder items-center gap-2">
+  <input type="text" 
+  className="grow outline-none"
+  placeholder="Search" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+</label>
   </div>
+  <div>
+    <label className="flex cursor-pointer gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+  <input type="checkbox" value="synthwave" className="toggle theme-controller"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+</label>
+  </div>
+  <div className="">
+    <a className="bg-black text-white px-3 py-3 rounded-md hover:bg-slate-800 duration-300 curser-pointer">Login</a>
+  </div>
+</div>
 </div>
     </div>
   )
