a52af9b683e66c754592cbcb1404556f52862c45
ansh-prasad <anshprasad01@gmail.com>
2024-05-22 13:54:57 +0530

    Cards and Cards Data in List file

---
+++ Projects/Book Store App/Frontend/src/components/Banner.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import banner from "../../public/Book.png";
+
+export const Banner = () => {
+  return (
+    <>
+        <div className='max-w-screen-2x1 cintainer mx-auto md:px-20 px-4 flex md:flex-row my-20'>
+            <div className='order-2 w-full md:w-1/2 mt-12 md:mt-32'>
+                <div className='space-y-12'>
+
+                    <h1 className='text-4xl font-bold'>
+                        Hello, Welcom Here To Learn Somtiong{" "}
+                        <span className='text-pink-500'>
+                            New Everday!!!
+                        </span>
+                    </h1>
+
+                    <p className='text-xl'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, et totam. Tempora amet atque expedita, quae corrupti totam sed pariatur corporis at veniam est voluptas animi!
+                    </p>
+                    
+                    <label className="input input-bordered flex items-center gap-2">
+                        Email
+                        <input type="text" className="grow" placeholder="daisy@site.com" />
+                    </label>
+                    
+                </div>
+
+               <button className="btn mt-6 btn-info">Submit</button>
+            </div>
+
+            <div className=' w-full md:w-1/2 px-2'>
+                <img src={banner} className="w-90 h-90" alt="" />
+            </div>
+        </div>
+    </>
+  )
+}
---
+++ Projects/Book Store App/Frontend/src/components/Cards.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Cards( {item}) {
+    
+  return (
+    <>
+        <div className='mt-6 my-3 p-3'>
+            <div className="card w-92 bg-base-100 shadow-xl">
+  <figure><img src={item.image} alt="" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      {item.name}
+      <div className="badge badge-secondary">{item.category}</div>
+    </h2>
+    <p>{item.tital}</p>
+    <div className="card-actions justify-between py-2 ">
+              <div className="badge badge-outline  ">${item.price}</div> 
+
+      <button className="btn btn-primary ">Buy Now</button>
+   
+    </div>
+  </div>
+</div>
+        </div>
+    </>
+  )
+}
+
+export default Cards
+
+
