c647dfb85e8304aee316f988547f502921ca0ed6
ansh-prasad <anshprasad01@gmail.com>
2024-05-22 13:54:09 +0530

    Nav Bar and Footer

--- Projects/Book Store App/Frontend/src/App.jsx
+++ Projects/Book Store App/Frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Banner } from './components/Banner'
+import Footer from './components/Footer'
+import FreeBook from './components/FreeBook'
 
 function App() {
   return (
-    <div><Navbar/></div>
+    <>
+     <Navbar/>
+     <Banner/>
+     <FreeBook/>
+
+     <Footer/>
+    </>
+   
   )
 }
 
